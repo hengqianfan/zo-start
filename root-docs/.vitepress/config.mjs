831128182b7808cc67a749ff762a5e16ab79e1d8
zocv <157280112+zocv@users.noreply.github.com>
2024-12-18 19:37:41 +0800

    0.0.8.
    .

--- root-docs/.vitepress/config.mjs
+++ root-docs/.vitepress/config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vitepress'
-
+// 引入全局通知组件
+import { AnnouncementPlugin } from 'vitepress-plugin-announcement'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "深岩焰界 | 阿牛宇宙",
   description: "A VitePress Site",
   base: '/zo-start/',
+  // base: '/',
   // 关闭深色模式
   appearance: false,
   head: [
@@ -24,5 +26,47 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     ]
+  },
+  vite: {
+    // ↓↓↓↓↓
+    plugins: [
+      AnnouncementPlugin({
+        title: '本站提示',
+        body: [
+          { type: 'text', content: '站长的能力有限，所以只有本站（zo-start）进行了服务器部署，其它项目都是托管到代码平台，如果你访问其它项目存在问题，' },
+
+          // {
+          //   type: 'image',
+          //   src: 'https://cdn.upyun.sugarat.top/mdImg/sugar/85c9554d023be2fcc5aab94effeef033',
+          //   style: 'display: inline-block;width:46%;padding-right:6px'
+          // },
+          // {
+          //   type: 'image',
+          //   src: 'https://cdn.upyun.sugarat.top/mdImg/sugar/54eacf3e730af9c1e3542a4800a422ea',
+          //   style: 'display: inline-block;width:46%;padding-left:6px'
+          // }
+        ],
+        footer: [
+          {
+            type: 'text',
+            content: '2024-12-19'
+          },
+          {
+            type: 'button',
+            content: '在线地址',
+            link: 'https://aniu.website'
+          },
+          {
+            type: 'button',
+            content: '仓库地址',
+            link: '',
+            props: {
+              type: 'success'
+            }
+          },
+        ],
+      })
+    ]
+    // ↑↑↑↑↑
   }
 })
