bb544e64c6b0022d0f27e4b86c44de639c554d22
zocv <157280112+zocv@users.noreply.github.com>
2024-12-19 14:27:47 +0800

    0.0.9.
    .

--- root-docs/.vitepress/config.mjs
+++ root-docs/.vitepress/config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
+import { links } from '../zo-data/link'
 // 引入全局通知组件
-import { AnnouncementPlugin } from 'vitepress-plugin-announcement'
+// import { AnnouncementPlugin } from 'vitepress-plugin-announcement'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "深岩焰界 | 阿牛宇宙",
@@ -16,57 +17,68 @@ export default defineConfig({
     logo: '/icon/logo.png',
 
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'details', link: '/intro' }
+      { text: 'home', link: '/' },
+      // { text: 'details', link: '/intro' },
+      { text: 'more', link: '/about' }
 
     ],
 
 
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://zocv.github.io/zo-start/' },
+
+      {
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="https://zocv.github.io/zo-notes/icon/png/logo.png"><title>Dribbble</title><path d="M12...6.38z"/></svg>'
+        }
+        ,
+        link: ''
+      }
     ]
   },
-  vite: {
-    // ↓↓↓↓↓
-    plugins: [
-      AnnouncementPlugin({
-        title: '本站提示',
-        body: [
-          { type: 'text', content: '站长的能力有限，所以只有本站（zo-start）进行了服务器部署，其它项目都是托管到代码平台，如果你访问其它项目存在问题，' },
+  // 公告插件配置 
 
-          // {
-          //   type: 'image',
-          //   src: 'https://cdn.upyun.sugarat.top/mdImg/sugar/85c9554d023be2fcc5aab94effeef033',
-          //   style: 'display: inline-block;width:46%;padding-right:6px'
-          // },
-          // {
-          //   type: 'image',
-          //   src: 'https://cdn.upyun.sugarat.top/mdImg/sugar/54eacf3e730af9c1e3542a4800a422ea',
-          //   style: 'display: inline-block;width:46%;padding-left:6px'
-          // }
-        ],
-        footer: [
-          {
-            type: 'text',
-            content: '2024-12-19'
-          },
-          {
-            type: 'button',
-            content: '在线地址',
-            link: 'https://aniu.website'
-          },
-          {
-            type: 'button',
-            content: '仓库地址',
-            link: '',
-            props: {
-              type: 'success'
-            }
-          },
-        ],
-      })
-    ]
-    // ↑↑↑↑↑
-  }
+  // vite: {
+  //   // ↓↓↓↓↓
+  //   plugins: [
+  //     AnnouncementPlugin({
+  //       title: '本站提示',
+  //       body: [
+  //         { type: 'text', content: '站长的能力有限，所以只有本站（zo-start）进行了服务器部署，其它项目都是托管到代码平台，如果你访问其它项目存在问题，' },
+
+  //         // {
+  //         //   type: 'image',
+  //         //   src: 'https://cdn.upyun.sugarat.top/mdImg/sugar/85c9554d023be2fcc5aab94effeef033',
+  //         //   style: 'display: inline-block;width:46%;padding-right:6px'
+  //         // },
+  //         // {
+  //         //   type: 'image',
+  //         //   src: 'https://cdn.upyun.sugarat.top/mdImg/sugar/54eacf3e730af9c1e3542a4800a422ea',
+  //         //   style: 'display: inline-block;width:46%;padding-left:6px'
+  //         // }
+  //       ],
+  //       footer: [
+  //         {
+  //           type: 'text',
+  //           content: '2024-12-19'
+  //         },
+  //         {
+  //           type: 'button',
+  //           content: '在线地址',
+  //           link: 'https://aniu.website'
+  //         },
+  //         {
+  //           type: 'button',
+  //           content: '仓库地址',
+  //           link: '',
+  //           props: {
+  //             type: 'success'
+  //           }
+  //         },
+  //       ],
+  //     })
+  //   ]
+  //   // ↑↑↑↑↑
+  // }
 })
--- root-docs/zo-data/link.js
+++ root-docs/zo-data/link.js
@@ -2,17 +2,17 @@ export const links = [
     {
         title: 'zo-notes',
         classone: '主世界',
-        intro: '阿牛的知识笔记库',
+        intro: '阿牛的知识笔记库，涵盖站长曾学习过、学习中的各领域内的知识笔记。',
         icon: '📔',
         link: 'https://zocv.github.io/zo-notes/'
 
     },
-    {
-        title: 'zo-life',
-        classone: '主世界',
-        intro: '记录俺',
-        icon: '🏚'
-    },
+    // {
+    //     title: 'zo-life',
+    //     classone: '主世界',
+    //     intro: '记录俺',
+    //     icon: '🏚'
+    // },
 
 
 
